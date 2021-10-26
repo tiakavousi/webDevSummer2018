fdc028e91b1e11ed5525273c380ab8fdfded037c
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-10-26 14:10:34 +0800

    reformed imports

--- src/main/java/com/example/webDevfall2021serverjavaTeya/models/Course.java
+++ src/main/java/com/example/webDevfall2021serverjavaTeya/models/Course.java
@@ -1,13 +1,14 @@
 package com.example.webDevfall2021serverjavaTeya.models;
 
-import java.sql.Date;
-
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+
 
 @Entity
 public class Course {
