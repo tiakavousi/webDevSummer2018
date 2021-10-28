5848a5d86eaf92756a2f2d7c98c9a27ca226f44c
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-10-28 09:06:31 +0800

    added new course service to create a course

--- src/main/java/com/example/webDevfall2021serverjavaTeya/services/CourseService.java
+++ src/main/java/com/example/webDevfall2021serverjavaTeya/services/CourseService.java
@@ -1,7 +1,10 @@
 package com.example.webDevfall2021serverjavaTeya.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webDevfall2021serverjavaTeya.models.Course;
@@ -11,9 +14,15 @@ import com.example.webDevfall2021serverjavaTeya.repositories.CourseRepository;
 public class CourseService {
 	@Autowired
 	CourseRepository courseRepository;
+	@CrossOrigin(origins="*")
+
 	@GetMapping("/api/course")
 	public Iterable<Course> findAllCourses() {
 		return courseRepository.findAll(); 
+	}
+	@PostMapping("/api/course")
+	public Course createCourse(@RequestBody Course course) {
+		return courseRepository.save(course);
 	}
 	
 }
