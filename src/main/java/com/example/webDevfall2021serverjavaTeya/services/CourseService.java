9e6d4f4a504fcd4ecf1f529263cd6bc377f9a6a0
Tayebe kavousi <tayebekavousi68@gmail.com>
2021-10-28 19:55:27 +0800

    added new api to delete a course

--- src/main/java/com/example/webDevfall2021serverjavaTeya/services/CourseService.java
+++ src/main/java/com/example/webDevfall2021serverjavaTeya/services/CourseService.java
@@ -2,7 +2,9 @@ package com.example.webDevfall2021serverjavaTeya.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,11 @@ public class CourseService {
 	@PostMapping("/api/course")
 	public Course createCourse(@RequestBody Course course) {
 		return courseRepository.save(course);
+	}
+	
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteCourse(@PathVariable("courseId")int id) {
+		courseRepository.deleteById(id);
 	}
 	
 }
